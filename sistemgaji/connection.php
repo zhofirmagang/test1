4e8f2db935ea7e13689b784327ffe2b50af1ad55
totoprayogo1916 <mylastof@gmail.com>
2020-08-27 16:19:18 +0700

    biarin aja mati

--- sistemgaji/connection.php
+++ sistemgaji/connection.php
@@ -7,5 +7,8 @@
          // Create connection
          $conn = mysqli_connect($servername, $username, $password,$dbname);
 
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
 
 ?>
